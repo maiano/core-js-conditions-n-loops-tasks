21d56ec0d02ae96c9bbdbd79c5a49cb4aec6e648
maiano <maiano@yandex.ru>
2024-12-08 21:58:15 +0100

    feat: 11 test pass

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -271,8 +271,13 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let divider = num;
+  while (divider > 0) {
+    if (divider % 10 === digit) return true;
+    divider = Math.floor(divider / 10);
+  }
+  return false;
 }
 
 /**
@@ -288,8 +293,24 @@ function isContainNumber(/* num, digit */) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  let sum = 0;
+  let left = 0;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    sum += arr[i];
+  }
+
+  for (let i = 0; i < arr.length; i += 1) {
+    sum -= arr[i];
+
+    if (left === sum) {
+      return i;
+    }
+    left += arr[i];
+  }
+
+  return -1;
 }
 
 /**
